f43b6a101b4e96a7fc59b653824fcf578ff1f2dd
Brandon Wells <brandon@brandonrwells.com>
2017-05-29 23:08:43 -0600

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -38,11 +38,8 @@ module.exports = {
   },
   devServer: {
     contentBase: "./public",
-    // do not print bundle build stats
     noInfo: true,
-    // enable HMR
     hot: true,
-    // embed the webpack-dev-server runtime into the bundle
     inline: true,
     historyApiFallback: true,
     port: PORT,
